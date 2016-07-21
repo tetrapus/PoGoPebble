a375a5c74e7d883bb8e9636a81e7827f6b0582e3
Joey Tuong <joey@tetrap.us>
2016-07-21 23:58:21 +1000

    i did stuff

--- src/app.js
+++ src/app.js
@@ -40,13 +40,12 @@ function updatePokemon() {
       type: 'json'
     },
     function(data, status, req) {
+      data = {"status":"success","pokemon":[{"id":"14152022","data":"[]","expiration_time":1469106527,"pokemonId":"19","latitude":-33.899361575357,"longitude":151.21056290263,"uid":"6b12b1e7b8f:46","is_alive":true},{"id":"14255985","data":"[]","expiration_time":1469106539,"pokemonId":"13","latitude":-33.899007263424,"longitude":151.21356817163,"uid":"6b12b1e6495:13","is_alive":true}]};
       console.log(data.pokemon.length);
       pokemon = data.pokemon;
       pokemon.sort(function(a, b) {
         return Distance.distance(position.coords, a) - Distance.distance(position.coords, b);
       });
-      
-      panel.on('click', 'select', updatePokemon);
       
       /*navigator.geolocation.watchPosition(
         setPosition,
@@ -67,6 +66,8 @@ navigator.geolocation.getCurrentPosition(
   function() { console.log('fetch location failed'); },
   geolocation_options
 );
+
+panel.on('click', 'select', updatePokemon);
 
 panel.add(background);
 panel.show();
